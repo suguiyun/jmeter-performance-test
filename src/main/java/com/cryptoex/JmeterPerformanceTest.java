6549599dc1be69b7f7bd1fc932f5784b4ccbb160
suguiyun <suguiyun@ziggurat.cn>
2018-08-30 22:03:45 +0800

    update

--- src/main/java/com/cryptoex/JmeterPerformanceTest.java
+++ src/main/java/com/cryptoex/JmeterPerformanceTest.java
@@ -1,4 +1,5 @@
 package com.cryptoex;
+
 import com.cryptoex.response.BusinessOrder;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
@@ -7,37 +8,41 @@ import org.apache.jmeter.samplers.SampleResult;
 
 import java.util.Map;
 
-public class JmeterPerformanceTest extends AbstractJavaSamplerClient{
+public class JmeterPerformanceTest extends AbstractJavaSamplerClient {
     private String inNum;
     private int inNumInt;
     private Client client;
+
     public Arguments getDefaultParameters() {
         Arguments params = new Arguments();
         // 添加一个新参数
         params.addArgument("inNum", "");
         return params;
     }
+
     public void setupTest(JavaSamplerContext jsc) {
-        client = new Client("AAAADBVJduGildYLPQ4NN3JU", "OZ6IprAoja0PusNM");
         inNum = jsc.getParameter("inNum", "");
         inNumInt = Integer.parseInt(inNum);
     }
+
     public SampleResult runTest(JavaSamplerContext arg0) {
+        JsonKey.Group group = (JsonKey.Group) JsonKey.list.get(Integer.valueOf(inNum)-1);
+        client = new Client(group.apiKey, group.apiSecret);
         SampleResult results = new SampleResult();
         try {
             results.sampleStart();
 
             Map<String, Object> orders;
-            if(inNumInt %2 == 0) {
-                orders = client.request(Map.class, null, "POST", "/v1/trade/orders", null, new BusinessOrder("0.001", "BUY_LIMIT", "1.2", "BTC_USDT"), null);
+            if (inNumInt % 2 == 0) {
+                orders = client.request(Map.class, null, "POST", "/v1/trade/orders", null, new BusinessOrder("1", "BUY_LIMIT", "1.2", "BTC_USDT"), null);
             } else {
-                orders = client.request(Map.class, null, "POST", "/v1/trade/orders", null, new BusinessOrder("0.001", "SELL_LIMIT", "1.2", "BTC_USDT"), null);
+                orders = client.request(Map.class, null, "POST", "/v1/trade/orders", null, new BusinessOrder("1", "SELL_LIMIT", "1.2", "BTC_USDT"), null);
             }
             results.setResponseData(orders.toString().getBytes());
 
             results.setResponseData(inNum.getBytes());
             results.setSuccessful(true);
-        } catch(Exception e) {
+        } catch (Exception e) {
             results.setResponseData(e.getMessage().getBytes());
             results.setSuccessful(false);
         } finally {
@@ -45,6 +50,7 @@ public class JmeterPerformanceTest extends AbstractJavaSamplerClient{
         }
         return results;
     }
+
     public void teardownTest(JavaSamplerContext arg0) {
     }
 }
